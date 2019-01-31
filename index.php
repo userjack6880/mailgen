fbd8ec31b9da285067d1caa62e132b5aeb2539f6
userjack6880 <john@systemanomaly.com>
2019-01-30 22:22:29 -0600

    did some updates I never pushed

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 		<meta name="viewport" content="width=device-width; initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Merriweather">
 		<style type="text/css">
 			body {
 				font-family: Merriweather, serif;
